cebfdf3ad313eb6b2aa53d1d1c6b884f2acdad0b
harsh <rathoreharsh304@gmail.com>
2024-06-10 19:00:30 +0530

    terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,71 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "hello_world" {
+  name = "hello-world-repo"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+resource "aws_ecs_cluster" "hello_world_cluster" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world_task" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "${aws_ecr_repository.hello_world.repository_url}:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "hello_world_service" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.hello_world_cluster.id
+  task_definition = aws_ecs_task_definition.hello_world_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["your-subnet-id"]  # Replace with your subnet ID
+    assign_public_ip = true
+  }
+
+  desired_count = 1
+}
